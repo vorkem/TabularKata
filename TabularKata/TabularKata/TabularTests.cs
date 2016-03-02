85b0a560daa9e2afad13f1e0cd398d6ff402a763
vorkem <insomnia23@gmail.com>
2016-03-02 21:19:08 +0000

    Added second test to ensure notes can be played, created list of notes to play which get added to one by one.

--- TabularKata/TabularKata/TabularTests.cs
+++ TabularKata/TabularKata/TabularTests.cs
@@ -9,9 +9,28 @@ namespace TabularKata
         public void WhenNoInputsAreSelectedThenNoStringsArePlayed()
         {
             var subject = new Music();
+            subject.AddNote("-");
+            subject.AddNote("-");
+            subject.AddNote("-");
+            subject.AddNote("-");
+            subject.AddNote("-");
             var result = subject.Tabulate();
 
             Assert.That(result, Is.EqualTo("|-----|"));
+        }
+
+        [Test]
+        public void WhenTheSecondFretIsPlayedOnTheThirdBeatThenTheTabIsCorrect()
+        {
+            var subject = new Music();
+            subject.AddNote("-");
+            subject.AddNote("-");
+            subject.AddNote("2");
+            subject.AddNote("-");
+            subject.AddNote("-");
+            var result = subject.Tabulate();
+
+            Assert.That(result, Is.EqualTo("|--2--|"));
         }
     }
 }
