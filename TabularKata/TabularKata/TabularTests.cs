27e61f2e745f1f9a0b92be605300ba036aa06895
vorkem <insomnia23@gmail.com>
2016-03-02 21:09:44 +0000

    Added initial test with a single string and no inputs

---
+++ TabularKata/TabularKata/TabularTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace TabularKata
+{
+    [TestFixture]
+    public class GivenAOneStringGuitar
+    {
+        [Test]
+        public void WhenNoInputsAreSelectedThenNoStringsArePlayed()
+        {
+            var subject = new Music();
+            var result = subject.Tabulate();
+
+            Assert.That(result, Is.EqualTo("|-----|"));
+        }
+    }
+}
